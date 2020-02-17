46273d8edb37159c2376229270e517b47f440787
Mehmet Durmaz <mehmetdurmazz@yahoo.com>
2020-02-17 13:23:37 +0300

    Initial project added.

---
+++ src/main/java/com/springframeworktraining/springgenericresttemplate/SpringGenericRestTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.springframeworktraining.springgenericresttemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGenericRestTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGenericRestTemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springframeworktraining/springgenericresttemplate/SpringGenericRestTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springframeworktraining.springgenericresttemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGenericRestTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
